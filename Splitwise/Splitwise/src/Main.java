72abce938e8d9c97f8ae45f6041dae4a59786a25
hrutvijchore1 <abhi20.iitkgp@gmail.com>
2025-03-22 12:43:26 +0530

    Splitwise

---
+++ Splitwise/Splitwise/src/Main.java
@@ -0,0 +1,436 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+
+class User {
+    private String id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public User(String id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
+
+abstract class Split {
+    private User user;
+    double amount;
+
+    public Split(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
+
+class EqualSplit extends Split {
+
+    public EqualSplit(User user) {
+        super(user);
+    }
+}
+
+class ExactSplit extends Split {
+
+    public ExactSplit(User user, double amount) {
+        super(user);
+        this.amount = amount;
+    }
+}
+
+class PercentSplit extends Split {
+    double percent;
+
+    public PercentSplit(User user, double percent) {
+        super(user);
+        this.percent = percent;
+    }
+
+    public double getPercent() {
+        return percent;
+    }
+
+    public void setPercent(double percent) {
+        this.percent = percent;
+    }
+}
+
+class ExpenseMetadata {
+    private String name;
+    private String imgUrl;
+    private String notes;
+
+    public ExpenseMetadata(String name, String imgUrl, String notes) {
+        this.name = name;
+        this.imgUrl = imgUrl;
+        this.notes = notes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
+
+abstract class Expense {
+    private String id;
+    private double amount;
+    private User paidBy;
+    private List<Split> splits;
+    private ExpenseMetadata metadata;
+
+    public Expense(double amount, User paidBy, List<Split> splits, ExpenseMetadata metadata) {
+        this.amount = amount;
+        this.paidBy = paidBy;
+        this.splits = splits;
+        this.metadata = metadata;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public User getPaidBy() {
+        return paidBy;
+    }
+
+    public void setPaidBy(User paidBy) {
+        this.paidBy = paidBy;
+    }
+
+    public List<Split> getSplits() {
+        return splits;
+    }
+
+    public void setSplits(List<Split> splits) {
+        this.splits = splits;
+    }
+
+    public ExpenseMetadata getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(ExpenseMetadata metadata) {
+        this.metadata = metadata;
+    }
+
+    public abstract boolean validate();
+}
+
+class EqualExpense extends Expense {
+    public EqualExpense(double amount, User paidBy, List<Split> splits, ExpenseMetadata expenseMetadata) {
+        super(amount, paidBy, splits, expenseMetadata);
+    }
+
+    @Override
+    public boolean validate() {
+        for (Split split : getSplits()) {
+            if (!(split instanceof EqualSplit)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+class ExactExpense extends Expense {
+    public ExactExpense(double amount, User paidBy, List<Split> splits, ExpenseMetadata expenseMetadata) {
+        super(amount, paidBy, splits, expenseMetadata);
+    }
+
+    @Override
+    public boolean validate() {
+        for (Split split : getSplits()) {
+            if (!(split instanceof ExactSplit)) {
+                return false;
+            }
+        }
+
+        double totalAmount = getAmount();
+        double sumSplitAmount = 0;
+        for (Split split : getSplits()) {
+            ExactSplit exactSplit = (ExactSplit) split;
+            sumSplitAmount += exactSplit.getAmount();
+        }
+
+        if (totalAmount != sumSplitAmount) {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+class PercentExpense extends Expense {
+    public PercentExpense(double amount, User paidBy, List<Split> splits, ExpenseMetadata expenseMetadata) {
+        super(amount, paidBy, splits, expenseMetadata);
+    }
+
+    @Override
+    public boolean validate() {
+        for (Split split : getSplits()) {
+            if (!(split instanceof PercentSplit)) {
+                return false;
+            }
+        }
+
+        double totalPercent = 100;
+        double sumSplitPercent = 0;
+        for (Split split : getSplits()) {
+            PercentSplit exactSplit = (PercentSplit) split;
+            sumSplitPercent += exactSplit.getPercent();
+        }
+
+        if (totalPercent != sumSplitPercent) {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+ enum ExpenseType {
+    EQUAL,
+    EXACT,
+    PERCENT
+}
+
+class ExpenseService {
+    public static Expense createExpense(ExpenseType expenseType, double amount, User paidBy, List<Split> splits, ExpenseMetadata expenseMetadata) {
+        switch (expenseType) {
+            case EXACT:
+                return new ExactExpense(amount, paidBy, splits, expenseMetadata);
+            case PERCENT:
+                for (Split split : splits) {
+                    PercentSplit percentSplit = (PercentSplit) split;
+                    split.setAmount((amount*percentSplit.getPercent())/100.0);
+                }
+                return new PercentExpense(amount, paidBy, splits, expenseMetadata);
+            case EQUAL:
+                int totalSplits = splits.size();
+                double splitAmount = ((double) Math.round(amount*100/totalSplits))/100.0;
+                for (Split split : splits) {
+                    split.setAmount(splitAmount);
+                }
+                splits.get(0).setAmount(splitAmount + (amount - splitAmount*totalSplits));
+                return new EqualExpense(amount, paidBy, splits, expenseMetadata);
+            default:
+                return null;
+        }
+    }
+}
+
+class ExpenseManager {
+    List<Expense> expenses;
+    Map<String, User> userMap;
+    Map<String, Map<String, Double> > balanceSheet;
+
+    public ExpenseManager() {
+        expenses = new ArrayList<Expense>();
+        userMap = new HashMap<String, User>();
+        balanceSheet = new HashMap<String, Map<String, Double>>();
+    }
+
+    public void addUser(User user) {
+        userMap.put(user.getId(), user);
+        balanceSheet.put(user.getId(), new HashMap<String, Double>());
+    }
+
+    public void addExpense(ExpenseType expenseType, double amount, String paidBy, List<Split> splits, ExpenseMetadata expenseMetadata) {
+        Expense expense = ExpenseService.createExpense(expenseType, amount, userMap.get(paidBy), splits, expenseMetadata);
+        expenses.add(expense);
+        for (Split split : expense.getSplits()) {
+            String paidTo = split.getUser().getId();
+            Map<String, Double> balances = balanceSheet.get(paidBy);
+            if (!balances.containsKey(paidTo)) {
+                balances.put(paidTo, 0.0);
+            }
+            balances.put(paidTo, balances.get(paidTo) + split.getAmount());
+
+            balances = balanceSheet.get(paidTo);
+            if (!balances.containsKey(paidBy)) {
+                balances.put(paidBy, 0.0);
+            }
+            balances.put(paidBy, balances.get(paidBy) - split.getAmount());
+        }
+    }
+
+    public void showBalance(String userId) {
+        boolean isEmpty = true;
+        for (Map.Entry<String, Double> userBalance : balanceSheet.get(userId).entrySet()) {
+            if (userBalance.getValue() != 0) {
+                isEmpty = false;
+                printBalance(userId, userBalance.getKey(), userBalance.getValue());
+            }
+        }
+
+        if (isEmpty) {
+            System.out.println("No balances");
+        }
+    }
+
+    public void showBalances() {
+        boolean isEmpty = true;
+        for (Map.Entry<String, Map<String, Double>> allBalances : balanceSheet.entrySet()) {
+            for (Map.Entry<String, Double> userBalance : allBalances.getValue().entrySet()) {
+                if (userBalance.getValue() > 0) {
+                    isEmpty = false;
+                    printBalance(allBalances.getKey(), userBalance.getKey(), userBalance.getValue());
+                }
+            }
+        }
+
+        if (isEmpty) {
+            System.out.println("No balances");
+        }
+    }
+
+    private void printBalance(String user1, String user2, double amount) {
+        String user1Name = userMap.get(user1).getName();
+        String user2Name = userMap.get(user2).getName();
+        if (amount < 0) {
+            System.out.println(user1Name + " owes " + user2Name + ": " + Math.abs(amount));
+        } else if (amount > 0) {
+            System.out.println(user2Name + " owes " + user1Name + ": " + Math.abs(amount));
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        ExpenseManager expenseManager = new ExpenseManager();
+
+        expenseManager.addUser(new User("u1", "User1", "gaurav@workat.tech", "9876543210"));
+        expenseManager.addUser(new User("u2", "User2", "sagar@workat.tech", "9876543210"));
+        expenseManager.addUser(new User("u3", "User3", "hi@workat.tech", "9876543210"));
+        expenseManager.addUser(new User("u4", "User4", "mock-interviews@workat.tech", "9876543210"));
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            String[] commands = command.split(" ");
+            String commandType = commands[0];
+
+            switch (commandType) {
+                case "SHOW":
+                    if (commands.length == 1) {
+                        expenseManager.showBalances();
+                    } else {
+                        expenseManager.showBalance(commands[1]);
+                    }
+                    break;
+                case "EXPENSE":
+                    String paidBy = commands[1];
+                    Double amount = Double.parseDouble(commands[2]);
+                    int noOfUsers = Integer.parseInt(commands[3]);
+                    String expenseType = commands[4 + noOfUsers];
+                    List<Split> splits = new ArrayList<>();
+                    switch (expenseType) {
+                        case "EQUAL":
+                            for (int i = 0; i < noOfUsers; i++) {
+                                splits.add(new EqualSplit(expenseManager.userMap.get(commands[4 + i])));
+                            }
+                            expenseManager.addExpense(ExpenseType.EQUAL, amount, paidBy, splits, null);
+                            break;
+                        case "EXACT":
+                            for (int i = 0; i < noOfUsers; i++) {
+                                splits.add(new ExactSplit(expenseManager.userMap.get(commands[4 + i]), Double.parseDouble(commands[5 + noOfUsers + i])));
+                            }
+                            expenseManager.addExpense(ExpenseType.EXACT, amount, paidBy, splits, null);
+                            break;
+                        case "PERCENT":
+                            for (int i = 0; i < noOfUsers; i++) {
+                                splits.add(new PercentSplit(expenseManager.userMap.get(commands[4 + i]), Double.parseDouble(commands[5 + noOfUsers + i])));
+                            }
+                            expenseManager.addExpense(ExpenseType.PERCENT, amount, paidBy, splits, null);
+                            break;
+                    }
+                    break;
+            }
+        }
+        }
+}
